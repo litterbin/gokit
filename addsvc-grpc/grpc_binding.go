6f53126da22a6580a52bdd5b0679c439f9adfb06
anarcher <anarcher@gmail.com>
2015-06-06 20:04:00 +0900

    modified:   grpc_binding.go

--- addsvc-grpc/grpc_binding.go
+++ addsvc-grpc/grpc_binding.go
@@ -9,9 +9,23 @@ import (
 	"golang.org/x/net/context"
 )
 
+// A binding wraps an Endpoint so that it's usable by a transport.
+// grpcBinding makes an Endpoint usable over gRPC.
 type grpcBinding struct{ endpoint.Endpoint }
 
 func (b grpcBinding) Add(ctx context.Context, req *pb.AddRequest) (*pb.AddReply, error) {
 	addReq := addRequest{req.A, req.B}
-	//    r,err :=
+	r, err := b.Endpoint(ctx, addReq)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, ok := r.(*addResponse)
+	if !ok {
+		return nil, endpoint.ErrBadCast
+	}
+
+	return &pb.AddReply{
+		V: resp.V,
+	}, nil
 }
